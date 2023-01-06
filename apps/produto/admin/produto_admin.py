209c26453175640198b5e76c357ce25331378ea6
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2023-01-06 12:03:21 -0300

    criando a model para produtos

---
+++ apps/produto/admin/produto_admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from ..models.produto import Produto
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = [
+        "nome",
+        "valor",
+        "estoque",
+        "criado",
+        "modificado",
+    ]
+
+    search_fields = [
+        "nome",
+    ]
