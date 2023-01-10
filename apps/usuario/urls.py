2e6f13c195ca6cd767baa90e6698b267e81794c9
Paulo0264 <fagundes.dev@hotmail.com>
2023-01-10 13:12:04 -0300

    Login em desenvolvimento

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,3 +1,12 @@
+from django import views
 from django.urls import path
 
-urlpatterns = []
+from usuario.views.login_view import LoginView
+from usuario.views.register_view import RegisterView
+
+app_name = "usuario"
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name="register"),
+    path('login/', LoginView.as_view(), name="login"),
+]
