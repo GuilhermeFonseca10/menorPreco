41c852f289f02806e2a99a32f589ecce84dd331e
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2023-01-16 16:40:48 -0300

    adicioinando o login

--- integrador/settings.py
+++ integrador/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static"),)
+STATIC_ROOT = os.path.join("static")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -144,3 +144,9 @@ AUTH_USER_MODEL = "usuario.Usuario"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+LOGIN_REDIRECT_URL = "home"
+LOGIN_URL = "login"
+AUTHENTICATION_BACKENDS = [
+    "authentication.LoginUsernameEmail",
+]
