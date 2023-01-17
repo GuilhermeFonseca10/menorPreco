4038187d795e3fb2cf5acfa8ad65f42105ba7cb0
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2023-01-16 21:52:26 -0300

    Adicionando o cadastro de usuarios

--- apps/usuario/views/register_view.py
+++ apps/usuario/views/register_view.py
@@ -1,5 +1,14 @@
-from django.views.generic.base import TemplateView
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from usuario.forms.usuario_form import UsuarioCreationForm
 
-class RegisterView(TemplateView):
+User = get_user_model()
 
+
+class RegisterView(CreateView):
+    model = User
+
+    form_class = UsuarioCreationForm
     template_name = "usuario/register.html"
+    success_url = reverse_lazy("home")
