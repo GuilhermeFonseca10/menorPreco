f1bb3d929d8b60e03bcea68371e9d41dd1149651
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2023-01-23 15:45:17 -0300

    retornando os dados dos supermercados

--- integrador/urls.py
+++ integrador/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path("usuario/", include("usuario.urls")),
     path("supermercado/", include("supermercado.urls")),
     path("", include("django.contrib.auth.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
