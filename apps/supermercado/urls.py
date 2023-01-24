ca01c10b72d0d6b84b0f0a9d00f74dea32f9a13a
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2023-01-23 21:32:48 -0300

    filtrando o usuario logado no dashboard

--- apps/supermercado/urls.py
+++ apps/supermercado/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
 from .views.supermercado_create import SupermercadoCreateView
-from .views.supermercado_dashboard import DashboardSupermercadoView
 from .views.supermercado_list import SupermercadoListView
 
 urlpatterns = [
     path("list", SupermercadoListView.as_view(), name="supermercado_list"),
     path("cad", SupermercadoCreateView.as_view(), name="supermercado_create"),
-    path("dashboard", DashboardSupermercadoView.as_view(), name="dashboard"),
 ]
