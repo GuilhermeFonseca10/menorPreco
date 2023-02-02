d185ea2eb8a25d50ba53fa5ace5661ba49d5e08a
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2023-02-02 17:35:51 -0300

    view de detalhes dos supermercados para o usuario

--- apps/supermercado/urls.py
+++ apps/supermercado/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views.supermercado_create import SupermercadoCreateView
 from .views.supermercado_delete import SupermercadoDeleteView
 from .views.supermercado_detail import SupermercadoDetailView
+from .views.supermercado_detail_usuario import SupermercadoUsuarioDetailView
 from .views.supermercado_list import SupermercadoListView
 from .views.supermercado_update import SupermercadoUpdateView
 
@@ -23,5 +24,10 @@ urlpatterns = [
         "delete/<int:pk>/",
         SupermercadoDeleteView.as_view(),
         name="supermercado_delete",
+    ),
+    path(
+        "detailusuario/<int:pk>/",
+        SupermercadoUsuarioDetailView.as_view(),
+        name="supermercado_detail_usuario",
     ),
 ]
---
+++ apps/supermercado/views/supermercado_detail_usuario.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import DetailView
+from supermercado.models.supermercado import Supermercado
+
+
+class SupermercadoUsuarioDetailView(DetailView):
+    model = Supermercado
+    template_name = "supermercado/supermercado_detail_usuario.html"
+
+    def supermercado_detail_usuario_view(request, id):
+        supermercados = Supermercado.objects.get(id=id)
+        # url = reverse("supermercado_detail", args=(id,))
+        context = {"supermercados": supermercados}
+        return render(request, context)
