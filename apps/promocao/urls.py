ce4dfa56e8dc174495e18879e4f483b686708df9
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2023-02-08 11:58:39 -0300

    adicionando editar e deletar para promocoes

--- apps/promocao/urls.py
+++ apps/promocao/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from promocao.views.promocao_create import PromocaoCreateView
+from promocao.views.promocao_delete import PromocaoDeleteView
 from promocao.views.promocao_detail import PromocaoDetailView
 from promocao.views.promocao_detail_admin import PromocaoAdminDetailView
 from promocao.views.promocao_list import PromocaoListView
+from promocao.views.promocao_update import PromocaoUpdateView
 
 urlpatterns = [
     path("cad", PromocaoCreateView.as_view(), name="promocao_create"),
@@ -16,5 +18,15 @@ urlpatterns = [
         "promocao_detail_admin/<int:pk>/",
         PromocaoAdminDetailView.as_view(),
         name="promocao_detail_admin",
+    ),
+    path(
+        "promocao_update/<int:pk>/",
+        PromocaoUpdateView.as_view(),
+        name="promocao_update",
+    ),
+    path(
+        "promocao_delete/<int:pk>/",
+        PromocaoDeleteView.as_view(),
+        name="promocao_delete",
     ),
 ]
